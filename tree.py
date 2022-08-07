23f0134eafb61e86efb5187eb2c5911932350471
DRK98519 <panlongxuderek@gmail.com>
2022-08-07 02:37:52 -0400

    Got the main structure done. Requires carefull debugging to ensure correctness

--- tree.py
+++ tree.py
@@ -1,10 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from shapely import geometry
+from numpy.linalg import norm
 
 
 # Treat Player state and Opponent state both in State class with different state.value
-def reach_set_calc(x_val, reach_range):     # With given x and reach_range, generate the Polygon that represents R(x)
+def reach_set_calc(x_val, reach_range):  # With given x and reach_range, generate the Polygon that represents R(x)
+    """
+    :type x_val: list
+    :type reach_range: int
+    :return: reach_set: Polygon
+    """
     p1 = geometry.Point(x_val[0] - reach_range, x_val[1] - reach_range)
     p2 = geometry.Point(x_val[0] + reach_range, x_val[1] - reach_range)
     p3 = geometry.Point(x_val[0] + reach_range, x_val[1] + reach_range)
@@ -21,7 +27,7 @@ def discrete_x_calc(poly, approx_para):
     :type approx_para: int
     :type poly: Polygon
     """
-    [hcoord_val, vcoord_val] = poly.exterior.xy     # Find the horizontal and vertical coordinates of poly's vertices
+    [hcoord_val, vcoord_val] = poly.exterior.xy  # Find the horizontal and vertical coordinates of poly's vertices
     discrete_x = []
     for x_hcoord in np.linspace(min(hcoord_val), max(hcoord_val), approx_para):
         for x_vcoord in np.linspace(min(vcoord_val), max(vcoord_val), approx_para):
@@ -32,16 +38,42 @@ def discrete_x_calc(poly, approx_para):
     # return t_node.children_state_list
     return discrete_x
 
+
+def cost_calc(i_t, x_t_minus_1):
+    """
+    :type i_t: State
+    :type x_t_minus_1: State
+    :return: cost_list: list
+    """
+    cost_list = []
+    for x_t in i_t.children_state_list:
+        x_t_vec = np.array(x_t.state)
+        x_t_minus_1_vec = np.array(x_t_minus_1.state)
+        cost_list.append(norm(x_t_vec - x_t_minus_1_vec, 2))
+    return cost_list
+
+
+# def cost_calc(previous_x_val, x_val):
+#     """
+#     :type previous_x_val: list
+#     :type x_val: list
+#     :return: cost_val: float
+#     """
+#     previous_x_val = np.array(previous_x_val)
+#     x_val = np.array(x_val)
+#     return norm(previous_x_val - x_val, 2)  # Use 2-norm as cost structure
+
+
 class State:
     # Set node loc i_t and parent_node i_t-1 as the attributes to newly defined OpNode object
     def __init__(self, state_val, parent_state, t_step, side):
         """
-            :type state_val: int (Opponent), list (Player)
-            :type parent_state: State / None (dummy_i)
-            :type t_step: int, None(dummy i)
-            :type side: str ('Player'/'Opponent')
-            """
-        self.value = state_val
+        :type state_val: int (Opponent), list (Player)
+        :type parent_state: State / None (dummy_i)
+        :type t_step: int, None(dummy i)
+        :type side: str ('Player'/'Opponent')
+        """
+        self.state = state_val
         self.side = side
         self.parent_state = parent_state
         self.children_state_list = []
@@ -49,6 +81,9 @@ class State:
 
     # Define methods that determine child nodes list with current node (Specific to graph)
     def add_child_state(self, child_state):
+        """
+        :type child_state: State
+        """
         # if self.value == 1:
         #     self.children_OpNode_list = [1, 3]
         # elif self.value == 2:
@@ -59,8 +94,11 @@ class State:
         #     print('Invalid node.')
         self.children_state_list.append(child_state)
 
-    def get_par_state(self):
-        return self.parent_state
+    # def cost_calc(self, previous_state):
+    #     x_t_minus_1_val = previous_state.value
+    #     x_t_val = self.value
+    #
+    #     return self.parent_state
 
 
 class ConvexBody:
@@ -75,12 +113,44 @@ class ConvexBody:
         self.region = geometry.Polygon(vertices)
 
 
+class Value:  # Value function
+    def __init__(self, player_state, oppo_state, t_step, side, value, action):
+        """
+        :type player_state: State (None for U0)
+        :type oppo_state: State (None for U0)
+        :type t_step: int
+        :type side: string
+        :type value: float
+        :type action: State
+        """
+        self.side = side
+        self.player_state = player_state
+        self.oppo_state = oppo_state
+        self.t_step = t_step
+        self.value = value
+        self.action = action
+
+
+# class Cost:
+#     def __init__(self, x_t_minus_1_val, x_t_val, cost_val):
+#         """
+#         :type x_t_minus_1_val: State
+#         :type x_t_val: State
+#         :type cost_val: float
+#         """
+#         self.x_t_minus_1_val = x_t_minus_1_val
+#         self.x_t_val = x_t_val
+#         self.cost_val = cost_val
+
+
 if __name__ == "__main__":
+    ################################################ Discretization ################################################
     state_queue = []
+    value_eval_queue = []  # My idea: Keep adding x and i states in discretization loop for later optimal value approximation. When running optimal value approximation, input opponent action, return optimal player action. Or, directly compute optimal player value and optimal opponent value function together with corresponding optimal player and opponent actions.
     dummy_i = State(state_val=None, parent_state=None, t_step=None, side=None)
     # Define given convex bodies Q
     Q = dict()
-    T = 2   # Finite horizon
+    T = 2  # Finite horizon
     vertices_list = {
         "t=0, i=1": [np.array([1, 1]), np.array([2, 1]), np.array([2, 2]), np.array([1, 2])],
         "t=0, i=2": [np.array([3, 3]), np.array([3, 2]), np.array([5, 2]), np.array([5, 3])],
@@ -98,7 +168,7 @@ if __name__ == "__main__":
     indx = 0
 
     # Define given Convex Bodies
-    for t in range(T+1):
+    for t in range(T + 1):
         for node in node_val:
             Q[f'Q_t={t}^i={node}'] = ConvexBody(t_step=t, node=node, vertices=vertices_list[f't={t}, i={node}'])
             hcoord, vcoord = Q[f'Q_t={t}^i={node}'].region.exterior.xy
@@ -121,51 +191,59 @@ if __name__ == "__main__":
     for i0_val in i0_vals:
         new_state = State(state_val=i0_val, parent_state=dummy_i, t_step=0, side='Opponent')
         dummy_i.add_child_state(new_state)
-        state_queue.append(new_state)   # Add all initial i_0 into state_queue, start expanding here
+        state_queue.append(new_state)  # Add all initial i_0 into state_queue, start expanding here
     print(state_queue)
 
-    while len(state_queue)>0:
+    while len(state_queue) > 0:
         # t_val = 0
         # while t_val < T:
-        expanding_state = state_queue.pop() # Choose the last element in state_queue by default
+        expanding_state = state_queue.pop()  # Choose the last element in state_queue by default
+        value_eval_queue.append(expanding_state)
+        # i_t_val = expanding_state.state
+        t_val = expanding_state.t_step
         # Find children states of initial Opponent state
         if expanding_state.t_step == 0 and expanding_state.side.lower() == 'opponent':
-            i_t_val = expanding_state.value
-            t_val = expanding_state.t_step
+            i_t_val = expanding_state.state
+            # t_val = expanding_state.t_step
             print(f'i_t_val={i_t_val}, t_val={t_val}, side={expanding_state.side}')
             disc_x_list = discrete_x_calc(Q[f'Q_t={t_val}^i={i_t_val}'].region, approx_para=2)
             for disc_x in disc_x_list:
+                # print(disc_x)
                 new_state = State(state_val=disc_x, parent_state=expanding_state, t_step=t_val, side='Player')
                 expanding_state.add_child_state(new_state)
                 state_queue.append(new_state)
         # Find children states of a Player state
         elif expanding_state.side.lower() == 'player':
-            i_t_val = expanding_state.parent_state.value
-            t_val = expanding_state.t_step
-            print(f'x_t_val={expanding_state.value}, t_val={t_val}, side={expanding_state.side}')
+            i_t_val = expanding_state.parent_state.state
+            # t_val = expanding_state.t_step
+            print(f'x_t_val={expanding_state.state}, t_val={t_val}, side={expanding_state.side}')
             if t_val != T:  # x_t is not terminal state if t != T
+                # Particular to given graph
                 if i_t_val == 1:
                     new_state_list = [1, 3]
                 elif i_t_val == 2:
                     new_state_list = [1, 3]
-                else:   # i_t_val == 3
+                else:  # i_t_val == 3
                     new_state_list = [2, 3]
 
                 for state_val in new_state_list:
-                    new_state = State(state_val, parent_state=expanding_state, t_step=t_val+1, side='Opponent')
+                    new_state = State(state_val, parent_state=expanding_state, t_step=t_val + 1, side='Opponent')
                     expanding_state.add_child_state(new_state)
                     state_queue.append(new_state)
             # x_t is terminal state if t = T, no children state for x_T
+            # if t_val > 0:
+
         elif expanding_state.side.lower() == 'opponent':
-            i_t_val = expanding_state.value
-            x_t_minus_1_val = expanding_state.parent_state.value
-            t_val = expanding_state.t_step
+            i_t_val = expanding_state.state
+            x_t_minus_1_val = expanding_state.parent_state.state
+            # t_val = expanding_state.t_step
+            # print(type(x_t_minus_1_val))
             print(f'x_t_minus_1_val={x_t_minus_1_val}, i_t_val={i_t_val}, t_val={t_val}, side={expanding_state.side}')
             R_set = reach_set_calc(x_t_minus_1_val, reach_range=20)
             R_intersect_Q = Q[f'Q_t={t_val}^i={i_t_val}'].region.intersection(R_set)
             # print(list(R_intersect_Q.exterior.coords))
             # R_intersect_Q = ConvexBody(t_val, i_t_val, list(R_intersect_Q.exterior.coords))
-            print(type(R_intersect_Q))
+            # print(type(R_intersect_Q))
             disc_x_list = discrete_x_calc(R_intersect_Q, approx_para=2)
             for disc_x in disc_x_list:
                 # print(disc_x)
@@ -174,9 +252,121 @@ if __name__ == "__main__":
                 state_queue.append(new_state)
         # print(state_queue)
         print('Running')
-    print('Done')
-    print(state_queue)
-    ################################################ Continue Here ################################################
+    print('Discretization Done')
+    # print(value_eval_queue.pop().parent_state.state)
+    # print(value_eval_queue.pop().parent_state.parent_state.state)
+    ################################################ End Here ################################################
+
+    ####################################### Optimal Value Approximation #######################################
+    # Get all leaf branches in the tree just for observation
+    out = [state for state in value_eval_queue if state.t_step == T and state.side.lower() == 'player']
+    # Remove all leaf branches in the tree
+    value_eval_queue = [state for state in value_eval_queue if state.t_step != T or state.side.lower() != 'player']
+    value_list = []
+    UV_dict = dict()
+    # for state in out:
+    #     print(state.children_state_list)
+    #     print(1)
+
+    # Test section for the code only (Delete later)
+    last_val_list = []
+    value_eval_queue1 = value_eval_queue
+    value_eval_queue2 = value_eval_queue
+    for count in range(10):
+        last_val_list.append(value_eval_queue1.pop())
+    for last_val in last_val_list:
+        print(last_val.state)
+        print(f'time step: {last_val.t_step}')
+        print(last_val.side)
+    print('\n\n\n')
+
+    # print(len(value_eval_queue))
+    while len(value_eval_queue) > 0:
+        # Two given states in value functions
+        given_state_1 = value_eval_queue.pop()
+        given_state_2 = given_state_1.parent_state
+        t_val = given_state_1.t_step
+        print(f"State 1: {given_state_1.state} t_val: {t_val} side: {given_state_1.side}")
+        print(f'State 2: {given_state_2.state} t_val: {given_state_2.t_step} side: {given_state_2.side}')
+
+        if t_val == T:  # Case of given x_T_minus_1 and i_T, select x_T
+            c_list = cost_calc(given_state_1, given_state_2)
+            # c_list = []
+            # # x_T_list = []
+            # for x_T in given_state_1.children_state_list:
+            #     cost_val = cost_calc(given_state_2.state, x_T.state)
+            #     # cost_val = norm(x_T.state - given_state_2.state, 2)
+            #     c_list.append(cost_val)
+            #     # cost = Value(given_state_2, given_state_1, t_val, 'Player', cost_val, action=x_T)
+            opt_val = min(c_list)  # Value of V_T
+            opt_indx = c_list.index(opt_val)
+            UV_dict[f'V_t={t_val} ({given_state_2.state}, {given_state_1.state})'] = \
+                Value(player_state=given_state_2, oppo_state=given_state_1, t_step=t_val, side='Player', value=opt_val,
+                      action=given_state_1.children_state_list[opt_indx])
+
+            print(f"side: {UV_dict[f'V_t={t_val} ({given_state_2.state}, {given_state_1.state})'].side}, "
+                  f"t_step: {t_val}")
+
+        elif given_state_1.side.lower() == 'player':    # Case of given x_t-1 and i_t-1, select i_t (1 <= t <= T)
+            V_t_val_list = []
+            for i_t in given_state_1.children_state_list:
+                V_t = UV_dict[f'V_t={t_val+1} ({given_state_1.state}, {i_t.state})']
+                V_t_val_list.append(V_t.value)
+
+            opt_val = max(V_t_val_list)
+            opt_indx = V_t_val_list.index(opt_val)
+            UV_dict[f'U_t={t_val+1} ({given_state_1.state}, {given_state_2.state})'] = \
+                Value(player_state=given_state_1, oppo_state=given_state_2, t_step=t_val+1, side='Opponent',
+                      value=opt_val, action=given_state_1.children_state_list[opt_indx])
+
+            print(f"side: {UV_dict[f'U_t={t_val+1} ({given_state_1.state}, {given_state_2.state})'].side}"
+                  f"t_step: {t_val+1}")
+
+        elif given_state_1.side.lower() == 'opponent':  # Case of given x_t-1 and i_t, select x_t. t!=T
+            t_val = given_state_1.t_step
+            if t_val != 0:  # 1 <= t < T (Discuss t = 0 case separately)
+                # V_t = min(c + U_t+1)
+                c_list = cost_calc(given_state_1, given_state_2)
+                U_t_plus_1_list = []
+                for x_t in given_state_1.children_state_list:
+                    U_t_plus_1_list.append(UV_dict[f'U_t={t_val+1} ({x_t.state}, {given_state_1.state})'].value)
+
+                val_array = np.array(c_list) + np.array(U_t_plus_1_list)
+                opt_val = min(val_array)     # V_t = min(c + U_t+1)
+                opt_indx = val_array.argmin()
+                UV_dict[f'V_t={t_val} ({given_state_2.state} {given_state_1.state})'] = \
+                    Value(player_state=given_state_2, oppo_state=given_state_1, t_step=t_val, side='Player',
+                          value=opt_val, action=given_state_1.children_state_list[opt_indx])
+
+                print(f"side: {UV_dict[f'V_t={t_val} ({given_state_2.state} {given_state_1.state})'].side}, "
+                      f"t_step: {t_val}")
+
+            else:   # t = 0 Given i_0, (Here, i_0.parent_state=dummy_i) find optimal x_0
+                U_1_list = []
+                for x_0 in given_state_1.children_state_list:
+                    U_1_list.append(UV_dict[f"U_t={t_val+1} ({x_0.state} {given_state_1.state})"].value)
+
+                opt_val = min(U_1_list)
+                opt_indx = U_1_list.index(opt_val)
+                UV_dict[f"V_t={t_val} ({given_state_2.state} {given_state_1.state})"] = \
+                    Value(player_state=given_state_2, oppo_state=given_state_1, t_step=t_val, side='Player',
+                          value=opt_val, action=given_state_1.children_state_list[opt_indx])
+
+                print(f"side: {UV_dict[f'V_t={t_val} ({given_state_2.state} {given_state_1.state})'].side}, "
+                      f"t_step: {t_val}")
+
+        # Case of no given states, referring to U_0
+        V_0_list = []
+        for i_0 in dummy_i.children_state_list:
+            V_0_list.append(UV_dict[f"V_t={0} ({dummy_i.state} {i_0.state})"].value)
+        opt_val = max(V_0_list)
+        opt_indx = V_0_list.index(opt_val)
+        UV_dict[f"U_t={0} ({dummy_i.state})"] = \
+            Value(player_state=None, oppo_state=None, t_step=0, side="Opponent",
+                  value=opt_val, action=dummy_i.children_state_list[opt_indx])
+
+        print(f"side: {UV_dict[f'U_t={0} ({dummy_i})'].side}, "
+              f"t_step: {0}")
 
     # node1 = State(state_val=1, parent_state=dummy_i, t_step=0, side='Opponent')
     # node2 = State(state_val=2, parent_state=dummy_i, t_step=0, side='Opponent')
@@ -213,7 +403,6 @@ if __name__ == "__main__":
     # for disc_x0 in disc_x0_list:
     #     new_state = State(state_val=disc_x0,parent_state=)
 
-
     # Plot multiple reachable set together in a figure
     # x = [np.array([1, 2]), np.array([2, 4]), np.array([43, 13])]
     # for x_coord in x:
@@ -226,7 +415,6 @@ if __name__ == "__main__":
     # plt.grid()
     # plt.show()
 
-
     # fig, axs = plt.subplots(1, 1)
     #
     # hcoord1, vcoord1 = reach_set_calc(x[0], 8).exterior.xy
@@ -235,10 +423,6 @@ if __name__ == "__main__":
     #
     # axs.fill(hcoord1, vcoord1, hcoord2, vcoord2, alpha=0.25, facecolor='red', edgecolor='red')
     # axs.grid(True)
-
-
-
-
 
 # class Node:
 #     def __init__(self, parent_node, point_loc):
@@ -272,5 +456,3 @@ if __name__ == "__main__":
 #             new_node = Node(parent_node=expanding_node, point_loc=point)
 #             expanding_node.add_children(new_node)
 #             node_queue.append(new_node)
-
-
