ff4bcc8edac55a289c7898beeec61da52739dafd
DRK98519 <panlongxuderek@gmail.com>
2022-09-26 16:47:43 -0400

    Fix coord_gen logical error

--- aCBC_demo.py
+++ aCBC_demo.py
@@ -248,7 +248,8 @@ def coord_gen(set, delta_X_t, disc_para, bound_rmv):
             if sides[0] == sides[1]:
                 coord_h = np.arange(bound_coord[0], bound_coord[2], delta_X_t)
                 coord_h += (1 / 2) * abs(bound_coord[2] - coord_h[-1])
-                coord_v = coord_h
+                coord_v = np.arange(bound_coord[1], bound_coord[3], delta_X_t)
+                coord_v += (1 / 2) * abs(bound_coord[3] - coord_v[-1])
                 """
                 print(f"coord_h: {coord_h}")
                 print(f"coord_v: {coord_v}")
